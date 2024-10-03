0ad26424a5f46b617c8ed53615e189e944f45a54
etherdevpro <etherjsdevpro@proton.me>
2024-10-04 04:14:13 +0900

    fix commit text issue

--- 2_4_Comments.php
+++ 2_4_Comments.php
@@ -8,12 +8,16 @@
     echo 'One Final Test'; # This is a one-line shell-style comment
 ?>
 
-<!-- The "one-line" comment styles only comment to the end of the line or the current block of PHP code, whichever comes first. This means that HTML code after // ... ?> or # ... ?> WILL be printed: ?> breaks out of PHP mode and returns to HTML mode, and // or # cannot influence that. -->
+<!-- The "one-line" comment styles only comment to the end of the line or the current block of PHP code, 
+ whichever comes first. This means that HTML code after // ... ?> or # ... ?> WILL be printed: ?> 
+ breaks out of PHP mode and returns to HTML mode, and // or # cannot influence that. -->
 
 <h1>This is an <?php # echo 'simple';?> example</h1>
 <p>The header above will say 'This is an  example'.</p>
 
-<!-- 'C' style comments end at the first */ encountered. Make sure you don't nest 'C' style comments. It is easy to make this mistake if you are trying to comment out a large block of code. -->
+<!-- 'C' style comments end at the first */ encountered. 
+ Make sure you don't nest 'C' style comments. 
+ It is easy to make this mistake if you are trying to comment out a large block of code. -->
 <?php
 /*  */
 ?>
